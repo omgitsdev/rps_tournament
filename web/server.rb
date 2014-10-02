cbcb53af8f9cb1134ec786f8ed4ce23792769834
Dev Sethi <omgitsdev@gmail.com>
2014-10-02 13:06:15 -0700

    starting game_populate method

--- web/server.rb
+++ web/server.rb
@@ -1,13 +1,21 @@
-require_relative '..lib/rps_tournament'
+require_relative '../lib/rps_tournament.rb'
 require 'sinatra/base'
 
 class RPS::Server < Sinatra::Application
   configure do
-    st :bind, '0.0.0.0'
+    set :bind, '0.0.0.0'
   end
 
   get '/' do
+  	@players = RPS::Player.all
     erb :index
   end
 
+  get '/tournaments/new' do
+  	@players = RPS::Player.all
+  	erb :new_tournament
+  end
+
+  post '/tournaments/new' do
+	end
 end
